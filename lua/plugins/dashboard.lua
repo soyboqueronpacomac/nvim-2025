a6e07781eb49be6cd35d04fb045c8e130436eeb6
Paco Coder <elboqueronpaco@icloud.com>
2025-08-04 13:51:05 +0200

    comandos para abrir la terminal

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,20 +1,34 @@
 return {
-  "folke/snacks.nvim",
-  event = "VimEnter",
-  opts = {
-    dashboard = {
-      header = {
-        [[ ██████╗  █████╗   ██████╗ ██████╗   ██████╗  ]],
-        [[ ██╔══██╗██╔══██╗ ██╔════╝ ██╔══██╗ ██╔═══██╗ ]],
-        [[ ██████╔╝███████║ ██║  ███╗██████╔╝ ██║   ██║ ]],
-        [[ ██╔═══╝ ██╔══██║ ██║   ██║██╔═══╝  ██║   ██║ ]],
-        [[ ██║     ██║  ██║ ╚██████╔╝██║      ╚██████╔╝ ]],
-        [[ ╚═╝     ╚═╝  ╚═╝  ╚═════╝ ╚═╝       ╚═════╝  ]],
-        [[           Paco-coder.nvim                   ]],
-      },
-      footer = {
-        " Listo para codificar",
-      },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
     },
+  },
+
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function()
+      local logo = [[
+██████   █████   ██████  ██████   ██████  ██████  ███████ ██████  
+██   ██ ██   ██ ██      ██   ██ ██       ██   ██ ██      ██   ██ 
+██████  ███████ ██      ██████  ██   ███ ██████  █████   ██████  
+██      ██   ██ ██      ██      ██    ██ ██      ██      ██   ██ 
+██      ██   ██  ██████ ██       ██████  ██      ███████ ██   ██ 
+                                                                   
+         PacoCoder-nvim-2025
+      ]]
+      logo = string.rep("\n", 2) .. logo .. "\n\n"
+
+      return {
+        theme = "doom",
+        config = {
+          header = vim.split(logo, "\n"),
+          footer = { "⚡ Listo para codificar" },
+        },
+      }
+    end,
   },
 }
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   opts = {
     size = 15,
-    open_mapping = [[<c-\>]],
+    open_mapping = [[<c-\>]], -- terminal flotante (estilo VSCode)
     hide_numbers = true,
     shade_terminals = true,
     shading_factor = 2,
@@ -12,8 +12,45 @@ return {
     insert_mappings = true,
     terminal_mappings = true,
     persist_size = true,
-    direction = "float", -- o "horizontal" o "vertical"
+    direction = "float", -- esta es la terminal por defecto (Ctrl+\)
     close_on_exit = true,
     shell = vim.o.shell,
   },
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+
+    -- Terminal horizontal
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    local horiz_term = Terminal:new({
+      direction = "horizontal",
+      hidden = true,
+      count = 1,
+    })
+
+    local vert_term = Terminal:new({
+      direction = "vertical",
+      hidden = true,
+      count = 2,
+    })
+
+    local dash_term = Terminal:new({
+      direction = "float",
+      hidden = true,
+      count = 3,
+      cmd = "htop", -- o cualquier comando útil
+    })
+
+    vim.keymap.set("n", "<C-h>", function()
+      horiz_term:toggle()
+    end, { desc = "Abrir terminal horizontal" })
+
+    vim.keymap.set("n", "<C-v>", function()
+      vert_term:toggle()
+    end, { desc = "Abrir terminal vertical" })
+
+    vim.keymap.set("n", "<C-t>", function()
+      dash_term:toggle()
+    end, { desc = "Abrir terminal tipo dashboard (htop)" })
+  end,
 }
